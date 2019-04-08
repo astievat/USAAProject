232d83bf4af882db018c3939741d364901ccd2fa
Ahenry9 <38470781+Ahenry9@users.noreply.github.com>
2019-04-07 18:29:04 -0700

    Add files via upload

--- js/getReviews.js
+++ js/getReviews.js
@@ -6,6 +6,7 @@ var reviewsArray;
 //this function grabs accounts and loads our account window
 function LoadReviews() {
 	var webMethod = "AccountServices.asmx/GetReviews";
+	var types = [];
 	$.ajax({
 		type: "POST",
 		url: webMethod,
@@ -18,7 +19,30 @@ function LoadReviews() {
 				//so we can use them in other functions as well
 				reviewsArray = msg.d;
 				//this clears out the div that will hold our account info
-				console.log(reviewsArray)
+				for(var obj in reviewsArray){
+					if(types.includes(reviewsArray[obj].qType)){
+						document.getElementById(reviewsArray[obj].qType).innerHTML += `
+							<ul class="nested" id='${reviewsArray[obj].qType}'>
+                                <li onclick=#>Report ${reviewsArray[obj].id}</li> <div id = "${reviewsArray[obj].id}" class="square"></div>
+                            </ul>`
+					}
+					else{
+						types.push(reviewsArray[obj].qType)
+                        document.getElementById('myUL').innerHTML +=
+                            `<li><span class="caret">${reviewsArray[obj].qType}</span>
+                                <ul class="nested" id='${reviewsArray[obj].qType}'>
+                                    <li onclick=#>Report ${reviewsArray[obj].id}</li> <div id = "${reviewsArray[obj].id}" class="square"></div>
+                                </ul>
+                            </li>`
+					}
+
+					
+
+				}
+
+
+
+
 				}
 		},
 		error: function (e) {
@@ -26,3 +50,5 @@ function LoadReviews() {
 		}
 	});
 }
+
+
